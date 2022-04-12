e53f55ff04351982423ffed519af30a737eec871
jimous <jimouspeng@xiaoe-tech.com>
2022-04-13 07:58:07 +0800

    fix

--- config/config.default.js
+++ config/config.default.js
@@ -36,8 +36,10 @@ module.exports = appInfo => {
             enable: false,
         },
         csrf: {
-            queryName: '_csrf', // 通过 query 传递 CSRF token 的默认字段为 _csrf
-            bodyName: '_csrf', // 通过 body 传递 CSRF token 的默认字段为 _csrf
+            enable: false,
+            headerName: 'token',
+            // queryName: '_csrf', // 通过 query 传递 CSRF token 的默认字段为 _csrf
+            // bodyName: '_csrf', // 通过 body 传递 CSRF token 的默认字段为 _csrf
         },
     }
 
