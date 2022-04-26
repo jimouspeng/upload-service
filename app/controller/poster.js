bf4a2586b948134245d1597f2607ccb25f6ccb87
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-04-26 15:18:54 +0800

    海报截图处理

--- app/controller/poster.js
+++ app/controller/poster.js
@@ -8,7 +8,9 @@
 
 const Controller = require('egg').Controller;
 const puppeteer = require('puppeteer');
+const render = require('art-template');
 const fs = require('fs');
+const path = require('path');
 
 class posterController extends Controller {
     async index() {
@@ -28,14 +30,15 @@ class posterController extends Controller {
             ],
         });
         const page = await browser.newPage();
-        await page.goto(
-            'https://t7.baidu.com/it/u=3997106521,2612372514&fm=193&app=53&size=w500&n=0&g=0n&f=jpeg?sec=1652861718&t=7670786bd6b87ea59534a2f516251303'
-        );
+        // await page.goto(
+        //     'https://t7.baidu.com/it/u=3997106521,2612372514&fm=193&app=53&size=w500&n=0&g=0n&f=jpeg?sec=1652861718&t=7670786bd6b87ea59534a2f516251303'
+        // );
         const picData = await page.screenshot({ path: 'test.png', type: 'jpeg', quality: 100 });
         console.log(picData);
         await browser.close();
         ctx.body = picData.toString();
     }
+    /** 海报生成 */
     async loaderPoster() {
         const { ctx } = this;
         console.log('业务进来了', ctx.service.user.find('001'));
@@ -51,12 +54,19 @@ class posterController extends Controller {
                 '–no-zygote',
                 '–single-process', // 单进程模式
             ],
+            defaultViewport: {
+                width: 375,
+                height: 862,
+            },
         });
         const page = await browser.newPage();
-        await page.goto(
-            'https://t7.baidu.com/it/u=3997106521,2612372514&fm=193&app=53&size=w500&n=0&g=0n&f=jpeg?sec=1652861718&t=7670786bd6b87ea59534a2f516251303'
-        );
-        const picData = await page.screenshot({ type: 'jpeg', quality: 80 });
+        /** 可以设置page.emulate(options)-模拟器视口配置 */
+        const getTemplate = 'http://127.0.0.1:2766/preview_poster';
+        // await page.goto(
+        //     'https://t7.baidu.com/it/u=3997106521,2612372514&fm=193&app=53&size=w500&n=0&g=0n&f=jpeg?sec=1652861718&t=7670786bd6b87ea59534a2f516251303'
+        // );
+        await page.goto(getTemplate);
+        const picData = await page.screenshot({ type: 'jpeg', quality: 80, fullPage: true });
         console.log(picData, ctx.app.baseDir);
         const file = fs.createWriteStream(`${ctx.app.baseDir}/images/test.jpg`);
         file.write(picData);
@@ -67,6 +77,15 @@ class posterController extends Controller {
         };
         ctx.body = JSON.stringify(body); /** ctx.body自动对buffer数据进行了组合, { data: bufferArray, type: buffer} */
         // ctx.body = picData
+    }
+    /** 海报预览 */
+    previewPoster() {
+        const { ctx } = this;
+        ctx.set('Content-Type', 'text/html');
+        const templatePath = path.resolve(ctx.app.baseDir, './app/template/index.art');
+        console.log(templatePath, '本地路径');
+        const templateData = render(templatePath, { pageData: { name: 'jimous' } });
+        ctx.body = templateData;
     }
 }
 
--- app/router.js
+++ app/router.js
@@ -8,10 +8,12 @@ module.exports = app => {
     router.get('/', controller.home.index);
     /** 测试post请求 */
     router.post('/get_info', controller.user.index);
-    /** 上传图片路由 */
+    /** 上传图片 */
     router.post('/upload_img', controller.upload.index);
-    /** 海报渲染路由 */
+    /** 海报渲染 */
     router.post('/get_poster', controller.poster.index);
-    /** 海报加载路由 */
+    /** 海报加载 */
     router.get('/loader_poster', controller.poster.loaderPoster);
+    /** 海报预览 */
+    router.get('/preview_poster', controller.poster.previewPoster);
 };
